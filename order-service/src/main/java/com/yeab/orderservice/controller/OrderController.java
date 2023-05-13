439d08a7979faecaddd6d5eb96bc565fe9652ed5
yeabLimtless <tesfayeyeabsra446@gmail.com>
2023-05-13 11:35:28 -0800

    add  resilience4J circuit breaker

--- order-service/src/main/java/com/yeab/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/yeab/orderservice/controller/OrderController.java
@@ -3,9 +3,13 @@ package com.yeab.orderservice.controller;
 
 import com.yeab.orderservice.dto.OrderRequest;
 import com.yeab.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,8 +20,12 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="inventory ")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+       return CompletableFuture.supplyAsync(() ->orderService.placeOrder(orderRequest));
+    }
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
+        return CompletableFuture.supplyAsync(() -> " OOps!!, Please order after some time  ");
     }
 }
